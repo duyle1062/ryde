1b827f4fc00069af04e58b6c41fde5de0aba999e
duyle1062 <22520315@gm.uit.edu.vn>
2025-09-07 22:42:02 +0700

    feat: Book Ride (update)

---
+++ components/Payment.tsx
@@ -0,0 +1,16 @@
+import CustomButton from "@/components/CustomButton";
+
+const Payment = () => {
+  const openPaymentSheet = async () => {};
+  return (
+    <>
+      <CustomButton
+        title="Confirm Ride"
+        className="my-10"
+        onPress={openPaymentSheet}
+      />
+    </>
+  );
+};
+
+export default Payment;
