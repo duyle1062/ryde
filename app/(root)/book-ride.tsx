e3ae59c298fef2e3d79cdba2b3bb0aa202d0db37
duyle1062 <22520315@gm.uit.edu.vn>
2025-09-07 09:55:03 +0700

    feat: Book Ride

---
+++ app/(root)/book-ride.tsx
@@ -0,0 +1,2 @@
+const BookRide = () => {};
+export default BookRide;
--- app/(root)/confirm-ride.tsx
+++ app/(root)/confirm-ride.tsx
@@ -1,5 +1,34 @@
+import RideLayout from "@/components/RideLayout";
+import { FlatList, View } from "react-native";
+import DriverCard from "@/components/DriverCard";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
+import { useDriverStore } from "@/store";
+
 const ConfirmRide = () => {
-  return ()
-}
+  const { drivers, selectedDriver, setSelectedDriver } = useDriverStore();
+  return (
+    <RideLayout title="Choose a Drivate" snapPoints={["65%", "85%"]}>
+      <FlatList
+        data={drivers}
+        renderItem={({ item }) => (
+          <DriverCard
+            selected={selectedDriver!}
+            setSelected={() => setSelectedDriver(item.id!)}
+            item={item}
+          />
+        )}
+        ListFooterComponent={() => (
+          <View className="mx-5 mt-10">
+            <CustomButton
+              title="Select Ride"
+              onPress={() => router.push("/(root)/book-ride")}
+            />
+          </View>
+        )}
+      />
+    </RideLayout>
+  );
+};
 
 export default ConfirmRide;
